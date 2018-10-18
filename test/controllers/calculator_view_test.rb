6322c9c5dab7c4d17813232cbe045fc2bf734c26
Wen Ting Wang <chubberlisk@outlook.com>
2018-10-18 16:17:11 +0100

    Made tests more specific by checking for data attribute value

--- test/controllers/calculator_view_test.rb
+++ test/controllers/calculator_view_test.rb
@@ -20,42 +20,48 @@ class CalculatorViewTest < ActionDispatch::IntegrationTest
 
   test 'shows a button to decrease life points of player one by 1000' do
     get calculator_url
-    assert_select 'button#lp-minus-one-thousand[data-lp-minus]'
+    assert_select 'button#lp-minus-one-thousand[data-lp-minus="1000"]'
   end
 
   test 'shows a button to decrease life points of player one by 500' do
     get calculator_url
-    assert_select 'button#lp-minus-five-hundred[data-lp-minus]'
+    assert_select 'button#lp-minus-five-hundred[data-lp-minus="500"]'
   end
 
   test 'shows a button to decrease life points of player one by 100' do
     get calculator_url
-    assert_select 'button#lp-minus-one-hundred[data-lp-minus]'
+    assert_select 'button#lp-minus-one-hundred[data-lp-minus="100"]'
   end
 
   test 'shows a button to decrease life points of player one by 10' do
     get calculator_url
-    assert_select 'button#lp-minus-ten[data-lp-minus]'
+    assert_select 'button#lp-minus-ten[data-lp-minus="10"]'
+  end
+
+  # Resetting the life points of player one
+  test 'shows a button to reset life points of player one' do
+    get calculator_url
+    assert_select 'button#lp-reset'
   end
 
   # Increasing life points of player one
   test 'shows a button to increase life points of player one by 1000' do
     get calculator_url
-    assert_select 'button#lp-plus-one-thousand[data-lp-plus]'
+    assert_select 'button#lp-plus-one-thousand[data-lp-plus="1000"]'
   end
 
   test 'shows a button to increase life points of player one by 500' do
     get calculator_url
-    assert_select 'button#lp-plus-five-hundred[data-lp-plus]'
+    assert_select 'button#lp-plus-five-hundred[data-lp-plus="500"]'
   end
 
   test 'shows a button to increase life points of player one by 100' do
     get calculator_url
-    assert_select 'button#lp-plus-one-hundred[data-lp-plus]'
+    assert_select 'button#lp-plus-one-hundred[data-lp-plus="100"]'
   end
 
   test 'shows a button to increase life points of player one by 10' do
     get calculator_url
-    assert_select 'button#lp-plus-ten[data-lp-plus]'
+    assert_select 'button#lp-plus-ten[data-lp-plus="10"]'
   end
 end

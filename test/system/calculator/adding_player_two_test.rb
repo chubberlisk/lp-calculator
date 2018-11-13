2a3f118512cd92bc1a52ae5155bd991b74c21b4d
Wen Ting Wang <chubberlisk@outlook.com>
2018-11-13 17:21:28 +0000

    Updated system test

--- test/system/calculator/adding_player_two_test.rb
+++ test/system/calculator/adding_player_two_test.rb
@@ -2,11 +2,8 @@ require 'application_system_test_case'
 
 class AddingPlayerTwoTest < ApplicationSystemTestCase
   # Adding player two
-  def setup
-    visit calculator_url
-  end
-
   test 'can add player two' do
+    visit calculator_url
     assert_no_selector 'button#remove-player-two'
     find('button#add-player-two').click
     assert_selector 'div#player-two-section', count: 1

55972337330763230c15ddef3055830bb9acfc88
Wen Ting Wang <chubberlisk@outlook.com>
2018-11-26 16:57:46 +0000

    improvement: if signed in, icon in LP Calculator takes user to their profile

--- test/system/calculator/returning_home_test.rb
+++ test/system/calculator/returning_home_test.rb
@@ -2,9 +2,17 @@ require 'application_system_test_case'
 
 class ReturningHomeTest < ApplicationSystemTestCase
   # Returning to LP Calculator home page
-  test 'can return to home page from calculator' do
+  test 'can return to home page from calculator if not signed in' do
     visit calculator_url
     find('a[href="/"]').click
     assert has_current_path?('/')
+  end
+
+  test 'can return to profile of player from calculator if signed in' do
+    user = create(:user)
+    login_as(user)
+    visit calculator_url
+    find("a[href='/users/#{user.id}']").click
+    assert has_current_path?("/users/#{user.id}")
   end
 end

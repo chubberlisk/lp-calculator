ef5c4c081711707a3fbc74123d5276f55140e4d5
Wen Ting Wang <chubberlisk@outlook.com>
2018-11-26 23:28:07 +0000

    refactor: use single quotation marks

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,18 +6,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @other_user = create(:user, email: 'monica@friends.com', username: 'monica')
   end
 
-  test "should get user profile if current user is viewing their own profile and they are signed in" do
+  test 'should get user profile if current user is viewing their own profile and they are signed in' do
     login_as(@user)
     get user_profile_path(@user)
     assert_response :success
   end
 
-  test "should not get user profile if user not signed in" do
+  test 'should not get user profile if user not signed in' do
     get user_profile_path(@user)
     assert_redirected_to new_user_session_path
   end
 
-  test "should not get user profile if user signed in and not viewing own user profile" do
+  test 'should not get user profile if user signed in and not viewing own user profile' do
     login_as(@user)
     get user_profile_path(@other_user)
     assert_response :unauthorized

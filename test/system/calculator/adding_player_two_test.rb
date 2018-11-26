daf60149a4788625101bbaf510b52f1523c26f05
Wen Ting Wang <chubberlisk@outlook.com>
2018-11-26 17:12:14 +0000

    test: add test for adding player two with account

--- test/system/calculator/adding_player_two_test.rb
+++ test/system/calculator/adding_player_two_test.rb
@@ -4,6 +4,7 @@ class AddingPlayerTwoTest < ApplicationSystemTestCase
   # Adding player two
   def setup
     @user = create(:user)
+    @other_user = create(:user, email: 'monica@friends.com', username: 'monica')
   end
 
   test 'can add player two as a guest when not signed in' do
@@ -28,6 +29,12 @@ class AddingPlayerTwoTest < ApplicationSystemTestCase
     find('button#add-player-two-guest').click
     # force modal close
     find('button#add-player-two-modal-close').click
+    within('h2#player-one') do
+      assert_text @user.username.upcase, options={:exact => true}
+    end
+    within('h2#player-two') do
+      assert_text 'PLAYER TWO', options={:exact => true}
+    end
     assert_selector 'div#player-two-section', count: 1
     assert_selector 'button#remove-player-two', count: 1
     assert_no_selector 'button#add-player-two'
@@ -42,5 +49,28 @@ class AddingPlayerTwoTest < ApplicationSystemTestCase
     find('button#add-player-two-account').click
     assert_selector 'input#player-two-email', count: 1
     assert_selector 'input#player-two-password', count: 1
+  end
+
+  test 'can add player two with an account when signed in' do
+    login_as(@user)
+    visit calculator_url
+    assert_no_selector 'button#remove-player-two'
+    find('button#add-player-two').click
+    find('button#add-player-two-account').click
+    fill_in('player-two[email]', with: @user.email)
+    fill_in('player-two[password]', with: @user.password)
+    click_on('Add')
+    # force modal close
+    find('button#add-player-two-modal-close').click
+    within('h2#player-one') do
+      assert_text @user.username.upcase, options={:exact => true}
+    end
+    within('h2#player-two') do
+      assert_text @other_user.username.upcase, options={:exact => true}
+    end
+    assert_selector 'div#player-two-section', count: 1
+    assert_selector 'button#remove-player-two', count: 1
+    assert_no_selector 'button#add-player-two'
+    assert_no_selector '#add-player-two-modal'
   end
 end

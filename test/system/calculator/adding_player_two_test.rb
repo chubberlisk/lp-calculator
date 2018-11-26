51faebc722149eb4fb084e5a352c791600b41092
Wen Ting Wang <chubberlisk@outlook.com>
2018-11-26 16:46:43 +0000

    improvement: Add Account button (and form) in LP Calculator only shown if signed in

--- test/system/calculator/adding_player_two_test.rb
+++ test/system/calculator/adding_player_two_test.rb
@@ -2,7 +2,26 @@ require 'application_system_test_case'
 
 class AddingPlayerTwoTest < ApplicationSystemTestCase
   # Adding player two
-  test 'can add player two as a guest' do
+  def setup
+    @user = create(:user)
+  end
+
+  test 'can add player two as a guest when not signed in' do
+    visit calculator_url
+    assert_no_selector 'button#remove-player-two'
+    find('button#add-player-two').click
+    assert_no_selector 'button#add-player-two-account'
+    find('button#add-player-two-guest').click
+    # force modal close
+    find('button#add-player-two-modal-close').click
+    assert_selector 'div#player-two-section', count: 1
+    assert_selector 'button#remove-player-two', count: 1
+    assert_no_selector 'button#add-player-two'
+    assert_no_selector '#add-player-two-modal'
+  end
+
+  test 'can add player two as a guest when signed in' do
+    login_as(@user)
     visit calculator_url
     assert_no_selector 'button#remove-player-two'
     find('button#add-player-two').click
@@ -15,7 +34,8 @@ class AddingPlayerTwoTest < ApplicationSystemTestCase
     assert_no_selector '#add-player-two-modal'
   end
 
-  test 'can view form to add player two with account' do
+  test 'can view form to add player two with account when signed in' do
+    login_as(@user)
     visit calculator_url
     assert_no_selector 'button#remove-player-two'
     find('button#add-player-two').click

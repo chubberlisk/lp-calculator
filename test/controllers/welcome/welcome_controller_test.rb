d255ff576baa4fbaaaa6d87802ee65d52c60d945
Wen Ting Wang <chubberlisk@outlook.com>
2018-11-26 23:27:43 +0000

    refactor: use single quotation marks

--- test/controllers/welcome/welcome_controller_test.rb
+++ test/controllers/welcome/welcome_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index if welcome_url named helper used" do
+  test 'should get index if welcome_url named helper used' do
     get welcome_url
     assert_response :success
   end
 
-  test "should get index if root_url named helper used" do
+  test 'should get index if root_url named helper used' do
     get root_url
     assert_response :success
   end

78129ccca34b6de4b59b25707e478c1a19b01bfc
Wen Ting Wang <chubberlisk@outlook.com>
2018-11-27 20:58:44 +0000

    improvement: create another user for DB seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(email: "batman@gmail.com", username: "batman", password: "password", password_confirmation: "password")
-AdminUser.create(email: "alfred@gmail.com", password: "password", password_confirmation: "password")
+User.create(email: "batman@gotham.com", username: "batman", password: "password", password_confirmation: "password")
+User.create(email: "robin@gotham.com", username: "robin", password: "password", password_confirmation: "password")
+AdminUser.create(email: "alfred@gotham.com", password: "password", password_confirmation: "password")

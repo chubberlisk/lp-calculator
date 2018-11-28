8f6ef5d9ad6194b55b8582c517c208834e5622d8
Wen Ting Wang <chubberlisk@outlook.com>
2018-11-28 15:40:14 +0000

    feat: easily find status of duel

--- app/models/duel.rb
+++ app/models/duel.rb
@@ -2,4 +2,6 @@ class Duel < ApplicationRecord
   belongs_to :player_one, class_name: 'User'
   belongs_to :player_two, class_name: 'User'
   has_many :users
+
+  enum status: { started: 0, completed: 1, cancelled: 2 }
 end
--- test/factories/duels.rb
+++ test/factories/duels.rb
@@ -1,5 +1,27 @@
 FactoryBot.define do
   factory :duel do
-    
+    status { 0 }
+    starting_lp { 8000 }
+    player_one_lp { nil }
+    player_two_lp { nil }
+    ended_at { nil }
+    association :player_one, factory: :chandler, strategy: :build
+    association :player_two, factory: :monica, strategy: :build
+
+    trait :started do
+      status { 0 }
+    end
+
+    trait :completed do
+      status { 1 }
+    end
+
+    trait :cancelled do
+      status { 2 }
+    end
+
+    factory :started_duel, traits: [:started]
+    factory :completed_duel, traits: [:completed]
+    factory :cancelled_duel, traits: [:cancelled]
   end
 end

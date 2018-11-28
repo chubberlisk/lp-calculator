f9ba7cb5edf689d1e8385ad0b9ede32f9b5231e4
Wen Ting Wang <chubberlisk@outlook.com>
2018-11-28 15:55:01 +0000

    feat: add duel id when player two with account added

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -9,12 +9,12 @@ class CalculatorController < ApplicationController
     @player_two = User.find_by(email: player_two_params[:player_two][:email])
     respond_to do |format|
       if @player_two && @player_two.valid_password?(player_two_params[:player_two][:password]) && !@player_two.eql?(@user)
-        duel = Duel.new(
+        @duel = Duel.new(
           player_one_id: @user.id, 
           player_two_id: @player_two.id,
           starting_lp: player_two_params[:starting_lp]
         )
-        @error = "Sorry, an error has occurred! Unable to create duel session." if !duel.save
+        @error = "Sorry, an error has occurred! Unable to create duel session." if !@duel.save
         format.js
       else
         @error = "Sorry! Invalid email or password."

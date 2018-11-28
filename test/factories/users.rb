0c80b88eb66112d836c5bec1927c6f2ee12c0b93
Wen Ting Wang <chubberlisk@outlook.com>
2018-11-28 15:41:11 +0000

    improvement: add factory for another user

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, aliases: [:chandler] do
     email { "chandler@friends.com" }
     username { "chandler" }
     password { "password" }
     password_confirmation { "password" }
+
+    factory :monica do
+      email { "monica@friends.com" }
+      username { "monica" }
+    end
   end
 end

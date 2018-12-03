ceec120b31f5b176ea5add8b98e9800590d8799e
Wen Ting Wang <chubberlisk@outlook.com>
2018-12-03 22:15:40 +0000

    test: add factories for duels with different winners

--- spec/factories/duels.rb
+++ spec/factories/duels.rb
@@ -8,23 +8,28 @@ FactoryBot.define do
     association :player_one, factory: :chandler, strategy: :build
     association :player_two, factory: :monica, strategy: :build
 
-    trait :started do
+    trait :completed do
+      ended_at { Time.now.advance(minutes: 30) }
+      status { 1 }
+    end
+
+    factory :started_duel do
       status { 0 }
     end
 
-    trait :completed do
-      ended_at { Time.now.advance(minutes: 30) }
+    factory :completed_duel, aliases: [:p1_winner_duel], traits: [:completed] do
       player_one_lp { 8000 }
       player_two_lp { 0 }
       status { 1 }
     end
 
-    trait :cancelled do
-      status { 2 }
+    factory :p2_winner_duel, traits: [:completed] do
+      player_one_lp { 0 }
+      player_two_lp { 8000 }
     end
 
-    factory :started_duel, traits: [:started]
-    factory :completed_duel, traits: [:completed]
-    factory :cancelled_duel, traits: [:cancelled]
+    factory :cancelled_duel do
+      status { 2 }
+    end
   end
 end

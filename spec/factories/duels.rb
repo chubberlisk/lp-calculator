6e7f3778a51a61400275c2898634a047e2ef133b
Wen Ting Wang <chubberlisk@outlook.com>
2018-12-10 15:45:04 +0000

    test: add factories for duels with different duel times

--- spec/factories/duels.rb
+++ spec/factories/duels.rb
@@ -4,6 +4,8 @@ FactoryBot.define do
     starting_lp { 8000 }
     player_one_lp { nil }
     player_two_lp { nil }
+    created_at { Time.now }
+    updated_at { Time.now }
     ended_at { nil }
     association :player_one, factory: :user_one, strategy: :build
     association :player_two, factory: :user_two, strategy: :build
@@ -17,10 +19,18 @@ FactoryBot.define do
       status { 0 }
     end
 
-    factory :completed_duel, aliases: [:p1_winner_duel], traits: [:completed] do
+    factory :completed_duel, aliases: [:p1_winner_duel, :thirty_mins_duel], traits: [:completed] do
       player_one_lp { 8000 }
       player_two_lp { 0 }
       status { 1 }
+    end
+
+    factory :fifteen_mins_duel, traits: [:completed] do
+      ended_at { Time.now.advance(minutes: 15) }
+    end
+
+    factory :sixty_mins_duel, traits: [:completed] do
+      ended_at { Time.now.advance(minutes: 60) }
     end
 
     factory :p2_winner_duel, traits: [:completed] do
